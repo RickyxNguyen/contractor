2b06c014c3c237608679cc96c0acec3f856be565
Ricky <rickywolf28@yahoo.com>
2019-10-07 15:16:41 -0700

    adding more readable codde

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
 import os
-
+from random import randint
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/KiWi')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
@@ -11,11 +10,11 @@ db = client.get_default_database()
 
 products = db.products
 products.drop()
-carts = db.cart
+carts = db.carts
 carts.drop()
 
-db.products.insert({"name":"WiBook Pro 15", "description":"Authentic Chinese Knockoff of MacBook Pro 15", "image":"./static/matebook.png"})
-db.products.insert({"name":"WiPro", "description":"Authentic Chinese Knockoff of Mac Pro", "image":"./static/grater.png"})
+db.products.insert_many([{"name":"WiBook Pro 15", "description":"Authentic Chinese Knockoff of MacBook Pro 15","price":3000, "image":"./static/matebook.png"},
+                        {"name":"WiPro", "description":"Authentic Chinese Knockoff of Mac Pro","price":10000, "image":"./static/grater.png"}])
 
 app = Flask(__name__)
 
@@ -40,11 +39,13 @@ def show_cart():
 def product_create(product_id):
     # This will delete a product by using an id as a parameter
     """Add one product to cart"""
-    product = products.find({'_id':ObjectId(product_id)})
-    
+
+
+    product = products.find_one({'_id':ObjectId(product_id)})
+
     carts.insert(product)
-    
-    return redirect(url_for('show_cart'))
+        
+    return redirect(url_for('show_cart', total=total))
 
 
 if __name__ == '__main__':
