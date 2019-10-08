a121c2bdc0008fe3f3a398ea127b13d4797d7f5b
Ricky <rickywolf28@yahoo.com>
2019-10-08 12:28:54 -0700

    can add more than one of the same item

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ products.drop()
 carts = db.carts
 carts.drop()
 
-db.products.insert_many([{"name":"WiBook Pro 15", "description":"Authentic Chinese Knockoff of MacBook Pro 15","price":2999.99, "image":"./static/matebook.png"},
-                        {"name":"WiPro", "description":"Authentic Chinese Knockoff of Mac Pro","price":9999.99, "image":"./static/grater.png"}])
+db.products.insert_many([{"name": "WiBook Pro 15", "description": "Authentic Chinese Knockoff of MacBook Pro 15", "price": 2999.99, "image": "./static/matebook.png"},
+                         {"name": "WiPro", "description": "Authentic Chinese Knockoff of Mac Pro", "price": 9999.99, "image": "./static/grater.png"}])
 
 app = Flask(__name__)
 
@@ -26,6 +26,7 @@ def show_products():
     # This will display all products by looping through the database
     return render_template('products_show.html', products=product)
 
+
 @app.route('/cart')
 def show_cart():
     """Show cart."""
@@ -34,10 +35,9 @@ def show_cart():
     total_price = list(carts.find({}))
     total = 0
     for i in range(len(total_price)):
-        total += total_price[i]["price"]
-        
-    return render_template('cart.html', carts=cart, total=total)
+        total += total_price[i]["price"]*total_price[i]["quantity"]
 
+    return render_template('cart.html', carts=cart, total=total)
 
 
 @app.route('/products/<product_id>/add', methods=['POST'])
@@ -45,32 +45,34 @@ def product_create(product_id):
     # This will delete a product by using an id as a parameter
     """Add one product to cart"""
 
-    if carts.count_documents({'_id':ObjectId(product_id)}) == 0:
+    if carts.find_one({'_id': ObjectId(product_id)}):
+        carts.update_one(
+            {'_id': ObjectId(product_id)},
+            {'$inc': {'quantity': int(1)}}
+        )
+    else:
+        carts.insert_one(
+            {**products.find_one({'_id': ObjectId(product_id)}), **{'quantity': 1}})
 
-        product = products.find_one({'_id':ObjectId(product_id)})
+    return redirect(url_for('show_cart'))
 
-        carts.insert(product)
-            
-        return redirect(url_for('show_cart'))
-
-    return redirect(url_for('out_of_stock'))
 
 @app.route('/carts/<cart_id>/delete', methods=['POST'])
 def cart_delete(cart_id):
     # This will delete a product by using an id as a parameter
     """Remove one product from cart"""
 
-    cart_item = carts.remove({'_id':ObjectId(cart_id)})
-        
+    carts.update_one(
+        {'_id': ObjectId(cart_id)},
+        {'$inc': {'quantity': -int(1)}}
+    )
     return redirect(url_for('show_cart'))
 
- 
+
 @app.route('/stock')
 def out_of_stock():
 
-
     return render_template('stock.html')
-
 
 
 if __name__ == '__main__':
